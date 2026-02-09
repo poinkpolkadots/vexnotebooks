a128b8c6f9ffcaf95d378fb6137e5c80f6e4b032
poinkpolkadots <olkadotspinkp@gmail.com>
2026-02-09 15:50:31 -0500

    created flask site to test uploading pdf paths

---
+++ Test_Site/init_db.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+    host = "drhscit.org",
+    database = os.environ["DB"],
+    user = os.environ["DB_UN"],
+    password = os.environ["DB_PW"]
+)
+
+cur = conn.cursor()
+
+cur.execute("DROP TABLE IF EXISTS notebooks;")
+cur.execute("CREATE TABLE notebooks (id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
+            "path TEXT,"
+            "output LONGTEXT,"
+            "date DATE DEFAULT CURRENT_DATE NOT NULL);"
+                                )
+
+conn.commit()
+
+cur.close()
+conn.close()
