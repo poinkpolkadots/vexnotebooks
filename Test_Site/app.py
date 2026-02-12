5604816620c47793b053480601948e5f13b3a7f7
poinkpolkadots <olkadotspinkp@gmail.com>
2026-02-12 09:51:56 -0500

    removed unecessary comments

--- Test_Site/app.py
+++ Test_Site/app.py
@@ -47,8 +47,6 @@ def create():
 
 @app.route("/delete/<int:id>/")
 def delete(id):
-    #Your code here - what should happen when a user clicks "Delete Review" on a particular review (with the specified id)? 
-    # runs a delete from statement where the id matches the inputted id
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("DELETE FROM notebooks WHERE id = %s", (id,))
@@ -56,18 +54,12 @@ def delete(id):
     cur.close()
     conn.close()
 
-    #Note - no need to change the code below - this will redirect the user back to the reviews page once they"ve deleted a review.
     return redirect(url_for("index"))
 
 @app.route("/edit/<int:id>", methods = ("GET", "POST"))
 def edit(id):
     #GET:
     if request.method == "GET":
-        #Your code here - what should happen when a user clicks "Edit Review" on a particular review (with the specified id)?
-        #in the form, the value attribute should be set to the existing review fields
-        #connect from database, select all where id matches
-        #modify render template statement to pass in data from table
-        #use fetchone() to get info and store in variable
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("SELECT * FROM notebooks WHERE id = %s", (id,))
@@ -75,13 +67,10 @@ def edit(id):
         cur.close()
         conn.close()
 
-        #Note - you will need to change the render_template code segment below to pass in information to the edit.html template (once you have modified edit.html).
         return render_template("edit.html", notebook = data)
     
     #POST:
     elif request.method == "POST":
-        #Your code here - what should happen when the user submits their edited review (for the review with the given id)?
-        # 
         name = request.form["name"]
 
         conn = get_db_connection()
@@ -92,7 +81,6 @@ def edit(id):
         conn.commit()
         cur.close()
         conn.close()
-        #Note - no need to change the code below - this will redirect the user back to the reviews page once they"ve submitted their edited review. 
         return redirect(url_for("index"))
 
 if __name__ == "__main__":
