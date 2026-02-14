be8b9f6f10008b2fc219360b4e95e0830602c8ec
LawrenceM2867 <hcps-morganla2@henricostudents.org>
2026-02-13 21:24:30 -0500

    multi file support

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import secrets
+import secrets, uuid
 from init_db import get_db_connection
+from werkzeug.utils import secure_filename
 from flask import *
 from data import *
 
@@ -20,18 +21,19 @@ def index():
 @app.route("/upload", methods=("GET", "POST"))
 def upload():
     if request.method == "POST":
-        file = request.files["file"]
-        if file:
-            name = file.filename
+        files = request.files.getlist("file")
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        for file in files:
+            name = secure_filename(f"{str(uuid.uuid4())[:4]}_{file.filename}")
             path = os.path.join(app.config["UPLOAD_FOLDER"], name)
             file.save(path)
-            
-            conn = get_db_connection()
-            cur = conn.cursor()
             cur.execute("INSERT INTO registry (name, path) VALUES (%s, %s)", (name, path))
-            conn.commit()
-            cur.close()
-            conn.close()
+        
+        conn.commit()
+        cur.close()
+        conn.close()
         return redirect(url_for("index"))
     return render_template("upload.html")
 
