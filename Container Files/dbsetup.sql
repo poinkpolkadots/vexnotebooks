bee44cfde28114b484d8383ce0c23ca694c72ae6
poinkpolkadots <olkadotspinkp@gmail.com>
2026-02-25 11:00:00 -0500

    added test file to test llama index with flask

--- Container Files/dbsetup.sql
+++ Container Files/dbsetup.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS notebooks;
 CREATE TABLE notebooks (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    notebook_name TEXT,
     pdf_path TEXT,
     output_path TEXT
 )
