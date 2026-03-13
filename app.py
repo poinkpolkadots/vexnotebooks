8be6a204ab30c7fdc3365c11c6df92974ade19a4
Rayhan Mohammed <hcps-mohammedr@henricostudents.org>
2026-03-13 18:42:39 -0400

    changed all html to flask

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import secrets
 from flask import Flask, render_template
-from util import *
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex(16)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/catalog/')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/notebookinfo/')
+def notebookinfo():
+    return render_template('notebookinfo.html')
+
+@app.route('/upload/')
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
